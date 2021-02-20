698182e37c6c21cb8c3a3803a29f86e774707a47
Adam <adam@bandhosting.nl>
2021-02-20 11:32:18 +0100

    added CSP test case

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    '/@': path.resolve(__dirname, './src')
+  },
+  optimizeDeps: {
+    exclude: [],
+    include: [],
+  },
   plugins: [vue()]
 })
